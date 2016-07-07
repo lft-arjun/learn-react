530de9f59e66eb758eea871f9c5c0b21cc7e2997
arjun <arjunsunar@lftechnology.com>
2016-07-07 10:06:21 +0545

    Completed first video tutorial with nested component

--- app/index.js
+++ app/index.js
@@ -1,19 +1,61 @@
 /**
  * Created by linux on 7/6/16.
  */
+var USER_DATA = {
+    name: 'Tyler MCGinnis',
+    username: 'tylermcginnis',
+    image: 'https://avatars2.githubusercontent.com/u/2933430?v=3&s=400',
+}
+
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function () {
-        return(
-            <div className='jumbotron'> Hello World </div>
 
-        )
+
+var ProfilePic = React.createClass({
+
+    render: function() {
+        return (
+            <img src={this.props.imageUrl} style={{height:100, width:100}} className="img-responsive img-thumbnail" />
+
+        );
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function() {
+        return (
+            <a href={'https://www.github.com/' + this.props.username}>
+            {this.props.username}
+        </a>
+        );
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        );
+    }
+});
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h2>React First Page</h2>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileLink username={this.props.user.username} />
+                <ProfileName name={this.props.user.name} />
+            </div>
+        );
     }
 });
 
 ReactDom.render(
-    <HelloWorld />,
+<Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
+
